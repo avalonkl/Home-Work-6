87c38e8f8945e0af028383d79cfc52158b142cad
Беляев Дмитрий <avalonkl@gmail.com>
2016-03-24 20:06:30 -0700

    Initial Commit

---
+++ Home Work 6/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Home Work 6
+//
+//  Created by Dmitriy Belyaev on 24.03.16.
+//  Copyright © 2016 Dmitriy Belyaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
